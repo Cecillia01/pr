492453127d014149206ae30f5d214fdbeb294f9f
c <c@gmail.com>
2024-09-30 22:34:45 +0800

    added multiplication method

--- calculator.java
+++ calculator.java
@@ -14,5 +14,9 @@ public class Calculator {
     public int subtraction(int a, int b) {
         return a - b;
     }
+    public int multiplication(int a, int b) {
+        return a * b;
+    }
+
 
 }
